c87fcaf2361b4eef9500776bbc56a535e3c8966b
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2020-09-25 02:28:18 +0100

    Storybook ok

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,10 @@
 module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
-  addons: [
-    "@storybook/react",
-    "@storybook/addon-actions",
-    "@storybook/addon-links"
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-};
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
